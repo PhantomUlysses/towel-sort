e4635d259c5bee6bdebf38d791c1bbd1cead43f9
Nikita Stasiulevich <nikitastas1694@gmail.com>
2021-03-08 23:40:05 +0300

    add not full solv

--- src/index.js
+++ src/index.js
@@ -2,5 +2,19 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  if (matrix == [] || matrix == undefined) {
+    return [];
+  } else {
+    matrix = matrix.sort();
+    let arr = [];
+    for (let i = 0; i < matrix.length; i++) {
+      if (i % 2 == 0) {
+        arr.push(matrix[i]);
+      } else {
+        arr.push(matrix[i].reverse());
+      }
+    }
+    let result = [];
+    return result.concat(...arr);
+  }
 }
